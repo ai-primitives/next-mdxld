be49dd58dac4fc91730d06b3a981ebd9b39b4d7e
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2024-12-23 05:12:37 -0600

    fix: got example working internally

--- examples/blog/next.config.js
+++ examples/blog/next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    urlImports: {
-      allowedUris: [
-        'https://esm.sh/**',
-        'https://cdn.skypack.dev/**',
-        'https://unpkg.com/**'
-      ]
-    }
-  },
-  components: {
-    // 'https://schema.org/BlogPosting': 'https://esm.sh/@mdxui/blog/components'
-  },
-  layouts: {
-    // Blog: 'https://esm.sh/@mdxui/blog/layouts/default'
+    mdxRs: true
   }
 }
 
